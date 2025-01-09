a991060f64c2b2fca5fdb8d592105acb2400e2d5
DWMike1 <mschuzcano-garcia@misena.edu.co>
2025-01-08 20:30:41 -0500

    Se eliminar archivos viejos, se actualizan secciones principales.

--- js/apps.js
+++ js/apps.js
@@ -37,3 +37,32 @@ if (target) {
     observer.observe(target);
 }
 });
+
+// funcion para ocultar tabla
+
+function showhide(){
+    var container = document.getElementsByClassName("plans-table")[0];
+
+    if(container.style.display == "none"){
+        container.style.display = "grid";
+    }else{
+        container.style.display = "none";
+    }
+}
+
+// funcion slider
+
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 3000); 
+}
